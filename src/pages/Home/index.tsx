41953be25e4f7056eba0209e4a9a53c05df0b87c
João Henrique Teixeira de Jesus <joaohenriquetdj@gmail.com>
2025-01-14 22:35:38 -0300

    Capitulo 2 Parte 7

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -10,13 +10,24 @@ export function Home(){
                     <TaskInput 
                         id="task"
                         placeholder="Dê um nome para o seu projeto"
+                        list="tasks"
                     />
+                    <datalist id="tasks">
+                        <option value="Projeto 1" />
+                        <option value="Projeto 2" />
+                        <option value="Projeto 3" />
+                        <option value="Projeto 4" />
+                    </datalist>
 
                     <label htmlFor="minutesAmount">durante</label>
                     <MinutesAmountInput 
                         type="number"
                         id="minutesAmount"
-                        placeholder="00"/>
+                        placeholder="00"
+                        step={5}
+                        min={5}
+                        max={60}
+                    />
                     <span>minutos.</span>
                 </FormContainer>
                 <CountDownContainer>

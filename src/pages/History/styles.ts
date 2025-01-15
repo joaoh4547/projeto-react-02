27044af7f9853312b0a8f1fefb70e68d2756ffb5
João Henrique Teixeira de Jesus <joaohenriquetdj@gmail.com>
2025-01-14 23:24:11 -0300

    Capitulo 1 Parte 9

--- src/pages/History/styles.ts
+++ src/pages/History/styles.ts
@@ -58,3 +58,29 @@ export const HistoryList = styled.main`
         }
     }
 `;
+
+const STATUS_COLORS = {
+    yellow: "yellow-500",
+    red: "red-500",
+    green: "green-500",
+};
+
+interface StatusPops{
+    statusColor: keyof typeof STATUS_COLORS
+}
+
+
+
+export const Status = styled.span<StatusPops>`
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+
+    &::before{
+        content: "";
+        width: 0.5rem;
+        height: 0.5rem;
+        border-radius: 50%;
+        background-color: ${p => p.theme[STATUS_COLORS[p.statusColor]]};
+    }
+`;

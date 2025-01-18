1a5829dd839ab723633f810f02bcde2dd7e035ef
João Henrique Teixeira de Jesus <joaohenriquetdj@gmail.com>
2025-01-18 17:38:05 -0300

    Capitulo 6 Parte 7

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -25,7 +25,7 @@ export function History(){
                                 <tr key={cycle.id}>
                                     <td>{cycle.task}</td>
                                     <td>{cycle.minutesAmount} minutos</td>
-                                    <td>{formatDistanceToNow(cycle.startDate,{ addSuffix: true, locale: ptBR})}</td>
+                                    <td>{formatDistanceToNow(new Date(cycle.startDate),{ addSuffix: true, locale: ptBR})}</td>
                                     <td>
                                         <Status statusColor={
                                             cycle.interruptDate ? "red" :

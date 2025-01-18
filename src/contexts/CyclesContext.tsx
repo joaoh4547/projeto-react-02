e43a6987b301874df8556936c1ffbc55f756b17c
João Henrique Teixeira de Jesus <joaohenriquetdj@gmail.com>
2025-01-18 16:59:42 -0300

    Capitulo 6 Parte 5

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -1,5 +1,6 @@
 import { createContext, ReactNode, useReducer, useState } from "react";
-import { ActionTypes, Cycle, cyclesReducer } from "../reducers/cycles";
+import { Cycle, cyclesReducer } from "../reducers/cycles/reducer";
+import { addNewCycleAction, interruptCurrentCycleAction, markCurrentCycleAsFinishedAction } from "../reducers/cycles/actions";
 
 
 
@@ -38,12 +39,7 @@ export function CyclesContextProvider({children}: CyclesContextProviderProps){
     const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
 
     function markCurrentCycleAsFinished(){
-        dispatch({
-            type: ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED,
-            payload:{
-                activeCycleId
-            }
-        });
+        dispatch(markCurrentCycleAsFinishedAction());
         
     }
 
@@ -59,24 +55,14 @@ export function CyclesContextProvider({children}: CyclesContextProviderProps){
             minutesAmount: data.minutesAmount,
             startDate: new Date(),
         };
-        dispatch({
-            type: ActionTypes.ADD_NEW_CYCLE,
-            payload:{
-                newCycle
-            }
-        });
+        dispatch(addNewCycleAction(newCycle));
         setAmountSecondsPass(0);
     }
 
 
 
     function interruptCurrentCycle(){
-        dispatch({
-            type: ActionTypes.INTERRUPT_CURRENT_CYCLE,
-            payload:{
-                activeCycleId
-            }
-        });
+        dispatch(interruptCurrentCycleAction());
     }
 
     return (
